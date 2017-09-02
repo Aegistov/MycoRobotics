d80ac4488b181b64d9b4d360a5d1dd8eb69b32af
Michael Morel <Aegis@Michaels-MacBook-Pro-2.local>
2017-09-01 21:44:34 -0400

    Modified SensorsWindow for Auto Layout implementation

--- src/scenes/Home/components/SensorsWindow/index.js
+++ src/scenes/Home/components/SensorsWindow/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Row, Col, Grid} from 'react-bootstrap';
 import Sensor from './components/Sensor';
+import * as firebase from 'firebase';
 import './style.css';
 
 class SensorsWindow extends Component {
@@ -16,6 +17,9 @@ class SensorsWindow extends Component {
       this.updateDimensions = this.updateDimensions.bind(this);
     }
 
+    let sensorQuantity = 0;
+    let sensorHeight = 0;
+
     componentDidMount() {
       this.updateDimensions();
       window.addEventListener('resize', this.updateDimensions);
@@ -30,10 +34,32 @@ class SensorsWindow extends Component {
       console.log("Class SensorsWindow\tInner width: " + window.innerWidth + "\tInner height: " + window.innerHeight);
     }
 
+    createSensorWindow() {
+      getSensorQuantity(function(){
+        sensorHeight = 100 / sensorQuantity;
+      });
+      console.log("Sensor Height: " + sensorHeight + "\tSensor Quantity: " + sensorQuantity);
+    }
+
+    getSensorQuantity() {
+      const sensorRef = firebase.database().ref("sensors");
+      sensorRef.once('value').then((snapshot) => {
+        sensorQuantity = snapshot.numChildren() - 2;
+        console.log("Number of Sensors: " + sensorQuantity);
+      });
+      console.log("Got sensor quantity.\nQuantity: " + sensorQuantity);
+    }
+
     render() {
+
+
+
+      createSensorWindow();
+
+      let sensorBlocks = [];
       return (
-        <Grid bsClass="Test" style={{height: this.props.height * 1}}>
-          <Row>
+        <Grid bsClass="SensorsWindow" style={{height: this.props.height * 1}}>
+          <Row bsClass="SColumn" style={{height: sensorHeight + "%"}}>
             <Col xs={6} id="SensorColumn">
               <Sensor sensor="00" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
             </Col>
@@ -58,15 +84,52 @@ class SensorsWindow extends Component {
             </Col>
           </Row>
           <Row>
-            <Col xs={4} id="SensorColumn">
-              <Sensor name="Temperature" sensor="AirTemperature" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="00" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
             </Col>
-            <Col xs={4} id="SensorColumn">
-              <Sensor name="Humidity" sensor="AirHumidity" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="01" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
             </Col>
-            <Col xs={4} id="SensorColumn">
-              <Sensor name="Carbon Dioxide" sensor="AirCarbonDioxide" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+          </Row>
+          <Row>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="00" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
             </Col>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="01" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="00" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+            </Col>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="01" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="00" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+            </Col>
+            <Col xs={6} id="SensorColumn">
+              <Sensor sensor="01" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+            </Col>
+          </Row>
+          <Row id="AirSensors">
+            <Row id="AirSensorsHeader">
+              <h6>Air Sensor</h6>
+            </Row>
+            <Row id="AirSensorsContent">
+              <Col xs={4} id="SensorColumn">
+                <Sensor name="Temperature" sensor="AirTemperature" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+              </Col>
+              <Col xs={4} id="SensorColumn">
+                <Sensor name="Humidity" sensor="AirHumidity" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+              </Col>
+              <Col xs={4} id="SensorColumn">
+                <Sensor name="Carbon Dioxide" sensor="AirCarbonDioxide" height={(this.props.height * .5)} width={(this.props.width * .7)}/>
+              </Col>
+            </Row>
           </Row>
         </Grid>
       );
